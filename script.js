7829b102108437cc77024311e5b0cd75984700e9
qubibi-official <seho2802@gmail.com>
2025-04-19 09:49:53 +0900

    qubibi_official_homepage_v1
    qubibi_official_homepage_v1

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const form = document.forms['submit-to-google-sheet'];
 const msg = document.getElementById('msg');
 
 form.addEventListener('submit', e => {
-    e.preventDefault();
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-        .then(response => {
-            msg.innerHTML = "Thank you for submitting!";
-            setTimeout(() => { msg.innerHTML = ""; }, 3000);
-            form.reset();
-        })
-        .catch(error => console.error('Error!', error.message))
+  e.preventDefault();
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+      msg.innerHTML = "Thanks for joining Qubibi!";
+      setTimeout(() => { msg.innerHTML = ""; }, 3000);
+      form.reset();
+    })
+    .catch(error => console.error('Error!', error.message));
 });
